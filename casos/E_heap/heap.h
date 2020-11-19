2a410f947f9b3ac486319afcb23defdb5e3ddfbb
LBanda <A01610329@itesm.mx>
2020-11-19 07:01:37 +0000

    heap

--- casos/E_heap/heap.h
+++ casos/E_heap/heap.h
@@ -39,34 +39,43 @@ public:
 
 template <class T>
 Heap<T>::Heap(unsigned int sze) throw (OutOfMemory) {
+  size = sze;
+	data = new T[size];
+	if (data == 0) {
+		throw OutOfMemory();
+	}
+	count = 0;
 }
 
 template <class T>
 Heap<T>::~Heap() {
+  delete [] data; data = 0;
+	size = 0; count = 0;
 }
 
 template <class T>
 bool Heap<T>::empty() const {
+  return (count == 0);
 }
 
 template <class T>
 bool Heap<T>::full() const {
-	return false;
+	return (count == size);
 }
 
 template <class T>
 unsigned int Heap<T>::parent(unsigned int pos) const {
-	return 0;
+	return (pos - 1) / 2;
 }
 
 template <class T>
 unsigned int Heap<T>::left(unsigned int pos) const {
-	return 0;
+	return ((2 * pos) + 1);
 }
 
 template <class T>
 unsigned int Heap<T>::right(unsigned int pos) const {
-	return 0;
+	return ((2 * pos) + 2);
 }
 
 template <class T>
@@ -78,21 +87,52 @@ void Heap<T>::swap(unsigned int i, unsigned int j) {
 
 template <class T>
 void Heap<T>::heapify(unsigned int pos) {
+  unsigned int le = left(pos);
+	unsigned int ri = right(pos);
+	unsigned int min = pos;
+	if (le <= count && data[le] < data[min]) {
+		min = le;
+	}
+	if (ri <= count && data[ri] < data[min]) {
+		min = ri;
+	}
+	if (min != pos) {
+		swap(pos, min);
+		heapify(min);
+	}
 }
 
 template <class T>
 void Heap<T>::add(T val) throw (Overflow) {
+  unsigned int pos;
+	if (full()) {
+		throw Overflow();
+	}
+
+	pos = count;
+	count++;
+	while (pos > 0 && val < data[parent(pos)]) {
+		data[pos] = data[parent(pos)];
+		pos = parent(pos);
+	}
+	data[pos] = val;
 }
 
 template <class T>
 T Heap<T>::remove() throw (NoSuchElement) {
-	T aux;
-	
+	if (empty()) {
+		throw NoSuchElement();
+	}
+	T aux = data[0];
+
+	data[0] = data[--count];
+	heapify(0);
 	return aux;
 }
 
 template <class T>
 void Heap<T>::clear() {
+  count = 0;
 }
 	
 
